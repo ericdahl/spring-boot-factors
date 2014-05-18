6e9d6b721a15696496d73f8ea21f9c2008f589a7
Eric Dahl <eric.c.dahl@gmail.com>
2014-05-18 11:40:01 -0500

    skeleton

---
+++ src/main/groovy/com/github/ericdahl/spring_boot_mersenne_primes/app.groovy
@@ -0,0 +1,15 @@
+package com.github.ericdahl.spring_boot_mersenne_primes
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+class app {
+     public static void main(String[] args) {
+         SpringApplication.run(app.class, args);
+     }
+}
