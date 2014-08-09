be7c943d7076054237bcce5d86e362a8c2fadab2
Eric Dahl <eric.c.dahl@gmail.com>
2014-08-09 12:46:46 -0700

    Update Spring Boot to 1.1.5-RELEASE

--- src/main/groovy/com/github/ericdahl/spring_boot_mersenne_primes/AppHealthIndicator.groovy
+++ src/main/groovy/com/github/ericdahl/spring_boot_mersenne_primes/AppHealthIndicator.groovy
@@ -1,6 +1,7 @@
 package com.github.ericdahl.spring_boot_mersenne_primes
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.cache.CacheManager
 import org.springframework.stereotype.Component
@@ -15,10 +16,12 @@ class AppHealthIndicator implements HealthIndicator {
     CacheManager cacheManager
 
     @Override
-    Map health() {
-        return [
-            'calculator': calculator.checkPrimality(7).prime,
-            'cacheManager': null != cacheManager.getCache("mersennePrimes").get(7, PrimeResult.class)
-        ]
+    Health health() {
+
+        return new Health.Builder()
+                .up()
+                .withDetail('calculator', calculator.checkPrimality(7).prime)
+                .withDetail('cacheManager', null != cacheManager.getCache("mersennePrimes").get(7, PrimeResult.class))
+                .build()
     }
 }
