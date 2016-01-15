f84225341a075baafe729325f72675455c31c838
Eric Dahl <eric.c.dahl@gmail.com>
2016-01-14 18:52:29 -0800

    fix imports

--- src/main/groovy/com/github/ericdahl/spring_boot_factors/app.groovy
+++ src/main/groovy/com/github/ericdahl/spring_boot_factors/app.groovy
@@ -1,16 +1,12 @@
 package com.github.ericdahl.spring_boot_factors
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.actuate.metrics.CounterService
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.support.SimpleCacheManager
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
 import org.springframework.web.filter.ShallowEtagHeaderFilter
 
 import javax.servlet.Filter
